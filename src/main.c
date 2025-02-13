a27a026514e1fbcbb281decca73dccad33483145
Anthony <arossi09@calpoly.edu>
2025-02-12 21:47:05 -0800

    Added View Matrix for Camera Controls

--- src/main.c
+++ src/main.c
@@ -14,6 +14,9 @@ struct TGAColor white = {255, 255, 255};
 struct TGAColor red = {255, 0, 0};
 struct TGAColor blue = {0, 0, 255};
 struct Vec3f camera = {0, 0, 3};
+struct Vec3f eye = {1, 1, 3};
+struct Vec3f center = {0,0,0};
+struct Vec3f up = {0, 1, 0};
 
 
 struct Vec3f m2v(struct Mat4 m){
@@ -33,6 +36,35 @@ struct Mat4 viewport(int x, int y, int w, int h ){
     m.data[2][2] = DEPTH/2.0f;
     return m;
 }
+struct Mat4 lookat(struct Vec3f eye, struct Vec3f center, struct Vec3f up){
+    struct Vec3f z = vector_sub3f(eye, center);
+    normalize3f(&z);
+    struct Vec3f x = vector_cross(up, z);
+    normalize3f(&x);
+    struct Vec3f y = vector_cross(z, x);
+    normalize3f(&y);
+    struct Mat4 res = identityMat4();
+
+    res.data[0][0] = x.x;
+    res.data[1][0] = y.x;
+    res.data[2][0] = z.x;
+
+    res.data[0][1] = x.y;
+    res.data[1][1] = y.y;
+    res.data[2][1] = z.y;
+
+    res.data[0][2] = x.z;
+    res.data[1][2] = y.z;
+    res.data[2][2] = z.z;
+
+    res.data[0][3] = -center.x;
+    res.data[1][3] = -center.y;
+    res.data[2][3] = -center.z;
+    return res;
+
+}
+
+
 struct Mat4 multMat4Vec4(struct Mat4 m, float x, float y, float z, float w) {
     struct Mat4 result = {0}; // Initialize all values to 0
 
@@ -184,30 +216,37 @@ void texTriangle(struct Vec3f *pts, struct Vec3f *txtPts, float *zbuffer,
 
 int main(int argc, char* argv[]){
 
+
     if(argc < 2){
         printf("Usage: ./prog filename.obj [diffuse.tga]");
         return -1;
     }
+
+    //load the diffuse texture if one was given
     struct TGA_image diffuseText;
     if(argc == 3){
         diffuseText = loadTGA(argv[2]);
     }
 
 
+    //load the model and create the TGA
     struct OBJ_Model model;
     struct TGA_image image = createTGA(WIDTH, HEIGHT, RGB);
     model = loadModel(argv[1]);
     if(model.nverts == 0){
         return -1;
     }
-    struct Vec3f light_dir = {0, 0, -1};
-    float zbuffer[WIDTH*HEIGHT] = {INT_MIN};
 
     printf("Drawing Faces...\n");
-
+    float zbuffer[WIDTH*HEIGHT] = {INT_MIN};
+    struct Mat4  ModelView = lookat(eye, center, up);
+    struct Vec3f light_dir = {0, 0, -1};
     struct Mat4 Projection = identityMat4();
+    //viewport is used for moving cordinates to screen cordiantes and scaling them
+    //appropriatley
     struct Mat4 ViewPort = viewport(WIDTH/8, HEIGHT/8, WIDTH*3/4, HEIGHT*3/4);
-    Projection.data[3][2] = -1.0f/camera.z;
+    struct Vec3f z = vector_sub3f(eye, center);
+    Projection.data[3][2] = -1.0f/magnitude3f(z);
 
 
     for(int i = 0; i < model.nfaces; i++){
@@ -223,7 +262,8 @@ int main(int argc, char* argv[]){
 
             //convert the world cords to align properly in our view
             //probably want to change this soon so vector multiplcation is more clean
-            screen_coords[j] = m2v(multMat4Vec4(multMat4(ViewPort, Projection), v.x, v.y, v.z, 1.0f));
+            //model and view matrix already multipled
+            screen_coords[j] = m2v(multMat4Vec4(multMat4(multMat4(ViewPort, Projection), ModelView), v.x, v.y, v.z, 1.0f));
             world_cords[j] = v;
             text_cords[j] = vt;
         }
